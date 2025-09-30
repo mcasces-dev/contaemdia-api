a5a112ccf11019c01adc85d6c0e7fe9bd4fb9048
mcasces-dev <michel.sousoia@gmail.com>
2025-09-30 17:45:14 -0300

    Update transacao.py

--- models/transacao.py
+++ models/transacao.py
@@ -1,1 +1,33 @@
+from datetime import datetime
+from typing import Literal
 
+class Transacao:
+    def __init__(self, descricao: str, valor: float, tipo: Literal['receita', 'despesa'], 
+                 categoria: str, data: str = None, id: int = None):
+        self.id = id
+        self.descricao = descricao
+        self.valor = valor
+        self.tipo = tipo
+        self.categoria = categoria
+        self.data = data if data else datetime.now().strftime("%d/%m/%Y %H:%M")
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'descricao': self.descricao,
+            'valor': self.valor,
+            'tipo': self.tipo,
+            'categoria': self.categoria,
+            'data': self.data
+        }
+    
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            id=data['id'],
+            descricao=data['descricao'],
+            valor=data['valor'],
+            tipo=data['tipo'],
+            categoria=data['categoria'],
+            data=data['data']
+        )
